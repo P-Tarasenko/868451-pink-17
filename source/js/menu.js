9cf3d2d7fa4c334e97e562c4476872c84fe6732d
P-Tarasenko <46486793+P-Tarasenko@users.noreply.github.com>
2019-08-18 22:49:05 +1000

    Доработка JS и добавление БЭМ сущностей
    * Убрал крестик на кнопке открытия меню без JS
    * Разметил секцию доступности приложения по БЭМ
    * Добавил переменные ширины вьюпорта

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,6 +2,7 @@ var header = document.querySelector(".main-header");
 var button = document.querySelector(".main-header__button");
 
 header.classList.add("main-header--close-menu");
+button.classList.add("main-header__button--show-cross");
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   header.classList.toggle("main-header--close-menu");
